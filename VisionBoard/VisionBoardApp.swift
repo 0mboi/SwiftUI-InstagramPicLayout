a9d1c3b2b78e7c11cf66a3f6c3323898a4a855e2
Christopher Hicks <visrule@protonmail.com>
2021-01-27 18:05:08 -0800

    Initial Commit

---
+++ VisionBoard/VisionBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionBoardApp.swift
+//  VisionBoard
+//
+//  Created by Christopher Hicks on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct VisionBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
